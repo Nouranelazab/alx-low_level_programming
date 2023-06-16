dd2c020a37dc938e0b95ca52f2103f6a93d688a6
Nouranelazab <nouranelazab54@gmail.com>
2023-06-16 13:29:52 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description: print numeric digits from 0 to 9
+ *              separated by ,, followed by a space
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit + '0');
+		putchar(',');
+		putchar(' ');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
