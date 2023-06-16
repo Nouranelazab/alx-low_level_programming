1163b54236e84db9f029c51686c2f7923dda62b3
Nouranelazab <nouranelazab54@gmail.com>
2023-06-16 13:18:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description: print prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+	char ch = 'a';
+
+	while (digit < 10)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+
+	while (ch <= f)
+	{
+		putchar(ch);
+		ch++;
+	}
+	printf("\n");
+	return (0);
+}
