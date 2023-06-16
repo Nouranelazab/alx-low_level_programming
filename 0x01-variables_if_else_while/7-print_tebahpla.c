afa2489d37be132fda6b6179c3dc64cb53d064c6
Nouranelazab <nouranelazab54@gmail.com>
2023-06-16 12:42:39 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description: print alphabetic letters from z to a
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
